24a63acf0c6e729639bda4b51f87da7b15e4dc78
Sean Currlin <sean.currlin@gmail.com>
2025-11-03 21:51:25 -0500

    install remaining deps

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import {auth} from "@/lib/better-auth/auth";
 import {headers} from "next/headers";
 import {redirect} from "next/navigation";
 
-const Layout = async ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children : React.ReactNode }) => {
     const session = await auth.api.getSession({ headers: await headers() });
 
     if(!session?.user) redirect('/sign-in');
