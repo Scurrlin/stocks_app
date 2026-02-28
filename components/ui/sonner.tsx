32af7b71b759d6f30230c78fcbe8c7df98e58dc3
Sean Currlin <sean.currlin@gmail.com>
2026-02-28 03:01:47 -0500

    update toast notifications again

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,27 +1,19 @@
 "use client"
 
-import { useTheme } from "next-themes"
 import { Toaster as Sonner, ToasterProps } from "sonner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
-
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme="light"
       className="toaster group"
       style={
         {
-          "--normal-bg": "var(--popover) !important",
-          "--normal-text": "var(--popover-foreground) !important",
-          "--normal-border": "var(--border) !important",
+          "--normal-bg": "#f0f0f0",
+          "--normal-text": "#000000",
+          "--normal-border": "#d4d4d4",
         } as React.CSSProperties
       }
-      toastOptions={{
-        classNames: {
-          description: "!text-current",
-        },
-      }}
       {...props}
     />
   )
